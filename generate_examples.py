047eb790a3e6f79bc9045de7f13b441ef9ae1275
zozoheir <zozoheir@umich.edu>
2024-11-25 03:01:34 -0500

    updated

---
+++ generate_examples.py
@@ -0,0 +1,6 @@
+example_path = '/home/othmane/Documents/repos/rumorz-ai/docs/api-reference/endpoint/example.mdx'
+
+def read_file_content(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
